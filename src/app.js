9843d9f494c0e57d78d5a768d333e3a637d21271
Dan Roberts <dan@adadevelopersacademy.org>
2017-11-28 12:28:28 -0800

    Read and write API, no error handling

--- src/app.js
+++ src/app.js
@@ -10,22 +10,8 @@ import _ from 'underscore';
 import BookList from './collections/book_list';
 
 const BOOK_FIELDS = ['title', 'author', 'publication_year'];
-const rawBookData = [
-  {
-    title: 'Practical Object-Oriented Design in Ruby',
-    author: 'Sandy Metz',
-    publication_year: 2012
-  }, {
-    title: 'Parable of the Sower',
-    author: 'Octavia Butler',
-    publication_year: 1993
-  }, {
-    title: 'A Wizard of Earthsea',
-    author: 'Ursula K. Le Guin',
-    publication_year: 1969
-  }
-];
-const bookList = new BookList(rawBookData);
+
+const bookList = new BookList();
 
 // Starts undefined - we'll set this in $(document).ready
 // once we know the template is available
@@ -63,16 +49,30 @@ const addBookHandler = function(event) {
   console.log("Read book data");
   console.log(bookData);
 
-  bookList.add(bookData);
+  const book = bookList.add(bookData);
+  book.save({}, {
+    success: (model, response) => {
+      console.log('Successfully saved book!');
+    },
+    error: (model, response) => {
+      console.log('Failed to save book! Server response:');
+      console.log(response);
+    },
+  });
 };
 
 $(document).ready(() => {
   bookTemplate = _.template($('#book-template').html());
 
-  render(bookList);
+  console.log(`About to fetch data from ${ bookList.url }`);
 
+  // Register our update listener first, to avoid the race condition
   bookList.on('update', render);
   bookList.on('sort', render);
+
+  // When fetch gets back from the API call, it will add books
+  // to the list and then trigger an 'update' event
+  bookList.fetch();
 
   // Listen for when the user adds a book
   $('#add-book-form').on('submit', addBookHandler);
