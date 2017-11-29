deb7af75e5eb361abbae02fa13bed94cdc9211f8
Dan Roberts <dan@adadevelopersacademy.org>
2017-11-29 10:52:33 -0800

    Add some custom model methods

--- src/models/book.js
+++ src/models/book.js
@@ -5,6 +5,29 @@ const Book = Backbone.Model.extend({
     author: 'Unknown'
   },
 
+  // None of these are very useful to us for this project
+  // initialize(attributes) {
+  //   console.log(`Book initialized with title ${ this.get('title')} and author ${this.get('author')}`);
+  //
+  //   console.log(attributes);
+  // },
+  //
+  // parse(serverData) {
+  //   // turn serverData into something Backbone can consume
+  // },
+  //
+  // toJSON() {
+  //
+  // },
+
+
+
+  age() {
+    return (new Date()).getFullYear() - this.get('publication_year');
+  },
+  toString() {
+    return `<Book ${ this.get('title') }>`;
+  },
 
 });
 
