4358decbeb2208425a6ef34f6c00b0a117e95841
dee <dee@adadevelopersacademy.org>
2017-11-28 17:05:43 -0800

    clean up of logs and some test code

--- src/app.js
+++ src/app.js
@@ -9,59 +9,32 @@ import Book from './models/book';
 import BookList from './collections/book_list';
 
 
-
-const codingInterview = new Book({
-  title: 'Cracking the Coding Interview',
-  author: 'Gale',
-  publication_year: 2011,
-  illustrator: 'Bob Ross',
-});
-
-// codingInterview.set('title', 'The Lord of the Flies');
-// console.log(`Title is ${codingInterview.get('title')}`);
-//
-//
-// console.log(codingInterview);
-
 const bookList = new BookList();
-bookList.add(codingInterview);
-
-// Starts undefined - we'll set this in $(document).ready
-// once we know the template is available
 let bookTemplate;
 
 const render = function render(bookList) {
 
-// Get the element to append to
-const $bookList = $('#book-list');
-$bookList.empty();
+  // Get the element to append to
+  const $bookList = $('#book-list');
+  $bookList.empty();
 
-// Build a forEach loop of bookList
-bookList.forEach((book) => {
-  $bookList.append(bookTemplate(book.attributes));
-});
-
-// bookList.forEach((book) => {
-//   $('#book-list').append(bookTemplate(book.attributes));
-// });
-  // Append with the template the book to the DOM
+  bookList.forEach((book) => {
+    $bookList.append(bookTemplate(book.attributes));
+  });
 };
 
 // Listening for a Backbone Event
 
 // 1.  Create an Event Listener
-
-const bogusListener = function bogusListener(event)  {
-  console.log('Event Occured!');
-  console.log(event);
-  console.log(this);
-};
+// const bogusListener = function bogusListener(event)  {
+//   console.log('Event Occured!');
+// };
 
 // 2.  Register the Event Handler with the Component
-bookList.on('bogus', bogusListener);
+// bookList.on('bogus', bogusListener);
 
 // 3.  Trigger the event
-bookList.trigger('bogus', 'Argument!');
+// bookList.trigger('bogus', 'Argument!');
 
 const fields = ['title', 'author', 'publication_year'];
 
@@ -72,8 +45,6 @@ const events = {
     fields.forEach( (field) => {
       bookData[field] = $(`input[name=${field}]`).val();
     });
-    console.log('Book Added');
-    console.log(bookData);
     const book = new Book(bookData);
     bookList.add(book);
     book.save({}, {
@@ -82,11 +53,9 @@ const events = {
     });
   },
   sortBooks(event) {
-    // remove current-sort-field from the class
-    // list of any element that has it.
     $('.current-sort-field').removeClass('current-sort-field');
-    // Add the class to the current selected element
     $(this).addClass('current-sort-field');
+
     // Get the class list of the selected element
     const classes = $(this).attr('class').split(/\s+/);
 
@@ -104,19 +73,12 @@ const events = {
     });
   },
   successfullSave(book, response) {
-    console.log('Success!');
-    console.log(book);
-    console.log(response);
     $('#status-messages ul').empty();
     $('#status-messages ul').append(`<li>${book.get('title')} added!</li>`);
     $('#status-messages').show();
   },
   failedSave(book, response) {
-    console.log('ERROR!');
-    console.log(book);
-    console.log(response);
     $('#status-messages ul').empty();
-    console.log(response.responseJSON.errors);
     for(let key in response.responseJSON.errors) {
       response.responseJSON.errors[key].forEach((error) => {
         $('#status-messages ul').append(`<li>${key}:  ${error}</li>`);
@@ -124,7 +86,6 @@ const events = {
     }
     $('#status-messages').show();
     book.destroy();
-
   },
 };
 
